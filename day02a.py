f25ae4dfdc6682b101d51277ddd7ba23b014373c
Marek Pšenka <psenka@edhouse.cz>
2023-11-01 22:01:51 +0100

    Day 02

--- day02a.py
+++ day02a.py
@@ -1,6 +1,15 @@
 import fileinput
+from collections import Counter
 
 input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+has_two = 0
+has_three = 0
+
+for c in map(Counter, input):
+    if any(map(lambda x: x == 2, c.values())):
+        has_two += 1
+    if any(map(lambda x: x == 3, c.values())):
+        has_three += 1
+
+print(has_two * has_three)
--- day02b.py
+++ day02b.py
@@ -1,6 +1,9 @@
 import fileinput
+import itertools
 
 input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+for [left, right] in itertools.combinations(input, 2):
+    common = list(filter(lambda p: p[0] == p[1], zip(left, right)))
+    if len(common) == len(left) - 1:
+        print("".join(map(lambda p: p[0], common)))
