7c8406fcd974738657061b397592205990ea95d8
Marek Pšenka <psenka@edhouse.cz>
2024-11-07 20:04:45 +0100

    Day 14

--- day14a.py
+++ day14a.py
@@ -2,5 +2,20 @@ import fileinput
 
 input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+req = int(next(input))
+
+scores = [3, 7]
+e1 = 0
+e2 = 1
+
+for i in range(req + 10):
+    sum_str = str(scores[e1] + scores[e2])
+    scores.append(int(sum_str[0]))
+    if len(sum_str) == 2:
+        scores.append(int(sum_str[1]))
+    e1 = (e1 + scores[e1] + 1) % len(scores)
+    e2 = (e2 + scores[e2] + 1) % len(scores)
+    if e1 == e2:
+        e2 = (e1 + 1) % len(scores)
+
+print("".join(map(str, scores[req : (req + 10)])))
--- day14b.py
+++ day14b.py
@@ -2,5 +2,20 @@ import fileinput
 
 input = map(str.rstrip, fileinput.input())
 
-for line in input:
-    print(line)
+req = [int(c) for c in next(input)]
+
+scores = [3, 7]
+e1 = 0
+e2 = 1
+
+while scores[-len(req) :] != req and scores[-len(req) - 1 : -1] != req:
+    sum_str = str(scores[e1] + scores[e2])
+    scores.append(int(sum_str[0]))
+    if len(sum_str) == 2:
+        scores.append(int(sum_str[1]))
+    e1 = (e1 + scores[e1] + 1) % len(scores)
+    e2 = (e2 + scores[e2] + 1) % len(scores)
+    if e1 == e2:
+        e2 = (e1 + 1) % len(scores)
+
+print(len(scores) - len(req) - (0 if scores[-len(req) :] == req else 1))
